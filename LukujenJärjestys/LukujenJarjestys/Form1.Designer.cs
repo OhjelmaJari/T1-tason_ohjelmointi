b9f41b5158d0d07b97a6df0752b496be96353a2f
Jari Kajanus <90889578+OhjelmaJari@users.noreply.github.com>
2021-09-28 17:51:53 +0300

    Lukujen Järjestys -tehtävä

---
+++ LukujenJärjestys/LukujenJarjestys/Form1.Designer.cs
@@ -0,0 +1,90 @@
+﻿
+namespace LukujenJarjestys
+{
+    partial class LukujenJarjestys
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.JarjestysLB = new System.Windows.Forms.Label();
+            this.uusiLukuTB = new System.Windows.Forms.TextBox();
+            this.VastausLB = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // JarjestysLB
+            // 
+            this.JarjestysLB.AutoSize = true;
+            this.JarjestysLB.Location = new System.Drawing.Point(47, 65);
+            this.JarjestysLB.Name = "JarjestysLB";
+            this.JarjestysLB.Size = new System.Drawing.Size(245, 30);
+            this.JarjestysLB.TabIndex = 0;
+            this.JarjestysLB.Text = "Anna luku (-999 lopetus):";
+            // 
+            // uusiLukuTB
+            // 
+            this.uusiLukuTB.Location = new System.Drawing.Point(342, 63);
+            this.uusiLukuTB.Name = "uusiLukuTB";
+            this.uusiLukuTB.Size = new System.Drawing.Size(175, 35);
+            this.uusiLukuTB.TabIndex = 1;
+            this.uusiLukuTB.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.uusiLukuTB_KeyPress);
+            // 
+            // VastausLB
+            // 
+            this.VastausLB.AllowDrop = true;
+            this.VastausLB.AutoSize = true;
+            this.VastausLB.Location = new System.Drawing.Point(342, 155);
+            this.VastausLB.Name = "VastausLB";
+            this.VastausLB.Size = new System.Drawing.Size(83, 30);
+            this.VastausLB.TabIndex = 2;
+            this.VastausLB.Text = "Vastaus";
+            this.VastausLB.UseWaitCursor = true;
+            this.VastausLB.Visible = false;
+            // 
+            // LukujenJarjestys
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(12F, 30F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(673, 252);
+            this.Controls.Add(this.VastausLB);
+            this.Controls.Add(this.uusiLukuTB);
+            this.Controls.Add(this.JarjestysLB);
+            this.Font = new System.Drawing.Font("Segoe UI", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.Margin = new System.Windows.Forms.Padding(5, 6, 5, 6);
+            this.Name = "LukujenJarjestys";
+            this.Text = "Lukujen Järjestys";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label JarjestysLB;
+        private System.Windows.Forms.TextBox uusiLukuTB;
+        private System.Windows.Forms.Label VastausLB;
+    }
+}
+
