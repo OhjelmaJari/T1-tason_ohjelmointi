7dd172b40a14e577bbad361c12a5f54ba41b6943
Jari Kajanus <90889578+OhjelmaJari@users.noreply.github.com>
2021-09-30 13:19:18 +0300

    30.9

---
+++ Painoindeksilaskuri/Form1.Designer.cs
@@ -0,0 +1,148 @@
+﻿
+namespace Painoindeksilaskuri
+{
+    partial class PainoindeksilaskuriForm
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.PainoTB = new System.Windows.Forms.TextBox();
+            this.PituusTB = new System.Windows.Forms.TextBox();
+            this.IndeksiLB = new System.Windows.Forms.Label();
+            this.KuvausLB = new System.Windows.Forms.Label();
+            this.LaskeBT = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Segoe UI", 36F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.label1.Location = new System.Drawing.Point(96, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(456, 65);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Painoindeksi (BMI)";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(123, 92);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(116, 25);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Painosi on ...";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(123, 125);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(129, 25);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "Pituutesi on ...";
+            // 
+            // PainoTB
+            // 
+            this.PainoTB.Location = new System.Drawing.Point(277, 89);
+            this.PainoTB.Name = "PainoTB";
+            this.PainoTB.Size = new System.Drawing.Size(100, 33);
+            this.PainoTB.TabIndex = 3;
+            // 
+            // PituusTB
+            // 
+            this.PituusTB.Location = new System.Drawing.Point(277, 125);
+            this.PituusTB.Name = "PituusTB";
+            this.PituusTB.Size = new System.Drawing.Size(100, 33);
+            this.PituusTB.TabIndex = 4;
+            // 
+            // IndeksiLB
+            // 
+            this.IndeksiLB.AutoSize = true;
+            this.IndeksiLB.Location = new System.Drawing.Point(123, 172);
+            this.IndeksiLB.Name = "IndeksiLB";
+            this.IndeksiLB.Size = new System.Drawing.Size(118, 25);
+            this.IndeksiLB.TabIndex = 5;
+            this.IndeksiLB.Text = "Painoindeksi";
+            this.IndeksiLB.Visible = false;
+            // 
+            // KuvausLB
+            // 
+            this.KuvausLB.AutoSize = true;
+            this.KuvausLB.Location = new System.Drawing.Point(123, 201);
+            this.KuvausLB.Name = "KuvausLB";
+            this.KuvausLB.Size = new System.Drawing.Size(72, 25);
+            this.KuvausLB.TabIndex = 6;
+            this.KuvausLB.Text = "Kuvaus";
+            this.KuvausLB.Visible = false;
+            // 
+            // LaskeBT
+            // 
+            this.LaskeBT.Location = new System.Drawing.Point(392, 107);
+            this.LaskeBT.Name = "LaskeBT";
+            this.LaskeBT.Size = new System.Drawing.Size(100, 31);
+            this.LaskeBT.TabIndex = 7;
+            this.LaskeBT.Text = "Laske";
+            this.LaskeBT.UseVisualStyleBackColor = true;
+            this.LaskeBT.Click += new System.EventHandler(this.LaskeBT_Click);
+            // 
+            // PainoindeksilaskuriForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(11F, 25F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(668, 274);
+            this.Controls.Add(this.LaskeBT);
+            this.Controls.Add(this.KuvausLB);
+            this.Controls.Add(this.IndeksiLB);
+            this.Controls.Add(this.PituusTB);
+            this.Controls.Add(this.PainoTB);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Font = new System.Drawing.Font("Segoe UI", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.Margin = new System.Windows.Forms.Padding(5, 5, 5, 5);
+            this.Name = "PainoindeksilaskuriForm";
+            this.Text = "Painoindeksilaskuri";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox PainoTB;
+        private System.Windows.Forms.TextBox PituusTB;
+        private System.Windows.Forms.Label IndeksiLB;
+        private System.Windows.Forms.Label KuvausLB;
+        private System.Windows.Forms.Button LaskeBT;
+    }
+}
+
---
+++ Painoindeksilaskuri/Form1.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Painoindeksilaskuri
+{
+    public partial class PainoindeksilaskuriForm : Form
+    {
+        public PainoindeksilaskuriForm()
+        {
+            InitializeComponent();
+        }
+
+        private void LaskeBT_Click(object sender, EventArgs e)
+        {
+            double paino = 0, pituus = 0;
+            paino = Convert.ToDouble(PainoTB.Text);
+            pituus = Convert.ToDouble(PituusTB.Text);
+            double BMI = Math.Round(paino/(pituus*pituus),2);
+            if(BMI < 18.5)
+            {
+                IndeksiLB.Text = "Painoindeksisi on: " + BMI;
+                IndeksiLB.ForeColor = Color.YellowGreen;
+                IndeksiLB.Visible = true;
+                KuvausLB.Text = "Alipaino";
+                KuvausLB.ForeColor = Color.YellowGreen;
+                KuvausLB.Visible = true;
+            }
+            else if (BMI < 25)
+            {
+                IndeksiLB.Text = "Painoindeksisi on: " + BMI;
+                IndeksiLB.ForeColor = Color.Green;
+                IndeksiLB.Visible = true;
+                KuvausLB.Text = "Normaalipaino";
+                KuvausLB.ForeColor = Color.Green;
+                KuvausLB.Visible = true;
+            }
+            else if (BMI < 40)
+            {
+                IndeksiLB.Text = "Painoindeksisi on: " + BMI;
+                IndeksiLB.ForeColor = Color.DeepPink;
+                IndeksiLB.Visible = true;
+                KuvausLB.Text = "Ylipaino";
+                KuvausLB.ForeColor = Color.DeepPink;
+                KuvausLB.Visible = true;
+            }
+            else
+            {
+                IndeksiLB.Text = "Painoindeksisi on: " + BMI;
+                IndeksiLB.ForeColor = Color.Red;
+                IndeksiLB.Visible = true;
+                KuvausLB.Text = "Huomattava ylipaino";
+                KuvausLB.ForeColor = Color.Red;
+                KuvausLB.Visible = true;
+            }
+        }
+    }
+}
