56516076c659cd7118fef0c4a08fe61ba06a1644
Jari Kajanus <90889578+OhjelmaJari@users.noreply.github.com>
2021-09-30 19:19:19 +0300

    30.9
    Tein hieman tutkimusta: siirsin "nollausfunktion" kutsun alkuun (hämmästyttävästi vaikutus kysymysnumeroon) ja lisäsin oikean rivin ja valittujen kohtien rivit. Valituksi tulee aina ensimmäiseksi "A" (viimeinen "D" jäi puuttumaan), teki miten vain, enkä saanut selvitettyä miksi.
    
    Palautuksessa muutettu versio.

---
+++ 10Kysymysta/Form1.Designer.cs
@@ -0,0 +1,215 @@
+﻿
+namespace _10Kysymysta
+{
+    partial class VastauslomakeForm
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.KysymysLB = new System.Windows.Forms.Label();
+            this.DummyRB = new System.Windows.Forms.RadioButton();
+            this.VastausARB = new System.Windows.Forms.RadioButton();
+            this.Vastaus = new System.Windows.Forms.GroupBox();
+            this.VastausDRB = new System.Windows.Forms.RadioButton();
+            this.VastausCRB = new System.Windows.Forms.RadioButton();
+            this.VastausBRB = new System.Windows.Forms.RadioButton();
+            this.VastausLB = new System.Windows.Forms.Label();
+            this.LB1 = new System.Windows.Forms.Label();
+            this.VLB1 = new System.Windows.Forms.Label();
+            this.VLB2 = new System.Windows.Forms.Label();
+            this.LB2 = new System.Windows.Forms.Label();
+            this.Vastaus.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // KysymysLB
+            // 
+            this.KysymysLB.AutoSize = true;
+            this.KysymysLB.Location = new System.Drawing.Point(12, 46);
+            this.KysymysLB.Name = "KysymysLB";
+            this.KysymysLB.Size = new System.Drawing.Size(208, 25);
+            this.KysymysLB.TabIndex = 0;
+            this.KysymysLB.Text = "Vastaus 1. kysymykseen";
+            // 
+            // DummyRB
+            // 
+            this.DummyRB.AutoCheck = false;
+            this.DummyRB.AutoSize = true;
+            this.DummyRB.Location = new System.Drawing.Point(46, 34);
+            this.DummyRB.Name = "DummyRB";
+            this.DummyRB.Size = new System.Drawing.Size(40, 29);
+            this.DummyRB.TabIndex = 5;
+            this.DummyRB.TabStop = true;
+            this.DummyRB.Text = "E";
+            this.DummyRB.UseVisualStyleBackColor = true;
+            this.DummyRB.Visible = false;
+            // 
+            // VastausARB
+            // 
+            this.VastausARB.AutoSize = true;
+            this.VastausARB.Location = new System.Drawing.Point(8, 32);
+            this.VastausARB.Name = "VastausARB";
+            this.VastausARB.Size = new System.Drawing.Size(42, 29);
+            this.VastausARB.TabIndex = 1;
+            this.VastausARB.TabStop = true;
+            this.VastausARB.Text = "A";
+            this.VastausARB.UseVisualStyleBackColor = true;
+            // 
+            // Vastaus
+            // 
+            this.Vastaus.Controls.Add(this.VastausDRB);
+            this.Vastaus.Controls.Add(this.VastausCRB);
+            this.Vastaus.Controls.Add(this.VastausBRB);
+            this.Vastaus.Controls.Add(this.DummyRB);
+            this.Vastaus.Controls.Add(this.VastausARB);
+            this.Vastaus.Location = new System.Drawing.Point(257, 12);
+            this.Vastaus.Name = "Vastaus";
+            this.Vastaus.Size = new System.Drawing.Size(92, 171);
+            this.Vastaus.TabIndex = 2;
+            this.Vastaus.TabStop = false;
+            this.Vastaus.Text = "Vastaus";
+            // 
+            // VastausDRB
+            // 
+            this.VastausDRB.AutoSize = true;
+            this.VastausDRB.Location = new System.Drawing.Point(8, 131);
+            this.VastausDRB.Name = "VastausDRB";
+            this.VastausDRB.Size = new System.Drawing.Size(43, 29);
+            this.VastausDRB.TabIndex = 4;
+            this.VastausDRB.TabStop = true;
+            this.VastausDRB.Text = "D";
+            this.VastausDRB.UseVisualStyleBackColor = true;
+            // 
+            // VastausCRB
+            // 
+            this.VastausCRB.AutoSize = true;
+            this.VastausCRB.Location = new System.Drawing.Point(8, 100);
+            this.VastausCRB.Name = "VastausCRB";
+            this.VastausCRB.Size = new System.Drawing.Size(42, 29);
+            this.VastausCRB.TabIndex = 3;
+            this.VastausCRB.TabStop = true;
+            this.VastausCRB.Text = "C";
+            this.VastausCRB.UseVisualStyleBackColor = true;
+            // 
+            // VastausBRB
+            // 
+            this.VastausBRB.AutoSize = true;
+            this.VastausBRB.Location = new System.Drawing.Point(8, 65);
+            this.VastausBRB.Name = "VastausBRB";
+            this.VastausBRB.Size = new System.Drawing.Size(41, 29);
+            this.VastausBRB.TabIndex = 2;
+            this.VastausBRB.TabStop = true;
+            this.VastausBRB.Text = "B";
+            this.VastausBRB.UseVisualStyleBackColor = true;
+            // 
+            // VastausLB
+            // 
+            this.VastausLB.AutoSize = true;
+            this.VastausLB.Location = new System.Drawing.Point(13, 147);
+            this.VastausLB.Name = "VastausLB";
+            this.VastausLB.Size = new System.Drawing.Size(76, 25);
+            this.VastausLB.TabIndex = 3;
+            this.VastausLB.Text = "Vastaus";
+            this.VastausLB.Visible = false;
+            // 
+            // LB1
+            // 
+            this.LB1.AutoSize = true;
+            this.LB1.Location = new System.Drawing.Point(13, 186);
+            this.LB1.Name = "LB1";
+            this.LB1.Size = new System.Drawing.Size(95, 25);
+            this.LB1.TabIndex = 4;
+            this.LB1.Text = "Oikea rivi:";
+            // 
+            // VLB1
+            // 
+            this.VLB1.AutoSize = true;
+            this.VLB1.Location = new System.Drawing.Point(12, 224);
+            this.VLB1.Name = "VLB1";
+            this.VLB1.Size = new System.Drawing.Size(70, 25);
+            this.VLB1.TabIndex = 14;
+            this.VLB1.Text = "Valitut:";
+            this.VLB1.Visible = false;
+            // 
+            // VLB2
+            // 
+            this.VLB2.AutoSize = true;
+            this.VLB2.Location = new System.Drawing.Point(114, 224);
+            this.VLB2.Name = "VLB2";
+            this.VLB2.Size = new System.Drawing.Size(54, 25);
+            this.VLB2.TabIndex = 18;
+            this.VLB2.Text = "VLB2";
+            this.VLB2.Visible = false;
+            // 
+            // LB2
+            // 
+            this.LB2.AutoSize = true;
+            this.LB2.Location = new System.Drawing.Point(114, 186);
+            this.LB2.Name = "LB2";
+            this.LB2.Size = new System.Drawing.Size(26, 25);
+            this.LB2.TabIndex = 11;
+            this.LB2.Text = "2:";
+            // 
+            // VastauslomakeForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(11F, 25F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(390, 411);
+            this.Controls.Add(this.VLB2);
+            this.Controls.Add(this.VLB1);
+            this.Controls.Add(this.LB2);
+            this.Controls.Add(this.LB1);
+            this.Controls.Add(this.VastausLB);
+            this.Controls.Add(this.Vastaus);
+            this.Controls.Add(this.KysymysLB);
+            this.Font = new System.Drawing.Font("Segoe UI", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.Margin = new System.Windows.Forms.Padding(5);
+            this.Name = "VastauslomakeForm";
+            this.Text = "Vastauslomake";
+            this.Load += new System.EventHandler(this.VastauslomakeForm_Load);
+            this.Vastaus.ResumeLayout(false);
+            this.Vastaus.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label KysymysLB;
+        private System.Windows.Forms.RadioButton DummyRB;
+        private System.Windows.Forms.RadioButton VastausARB;
+        private System.Windows.Forms.GroupBox Vastaus;
+        private System.Windows.Forms.RadioButton VastausDRB;
+        private System.Windows.Forms.RadioButton VastausCRB;
+        private System.Windows.Forms.RadioButton VastausBRB;
+        private System.Windows.Forms.Label VastausLB;
+        private System.Windows.Forms.Label LB1;
+        private System.Windows.Forms.Label VLB1;
+        private System.Windows.Forms.Label VLB2;
+        private System.Windows.Forms.Label LB2;
+    }
+}
+
---
+++ 10Kysymysta/Form1.cs
@@ -0,0 +1,114 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace _10Kysymysta
+{
+    public partial class VastauslomakeForm : Form
+    {
+        int Laskuri = 0;
+        int Oikein = 0;
+        string[] OikeatVastaukset = new string[] { "B", "D", "A", "A", "C", "A", "B", "A", "C", "D" };
+        string[] Vastaukset = new string[10];
+        string Annetut="", Oikeat="";
+
+        public VastauslomakeForm()
+        {
+            InitializeComponent();
+            VastausARB.CheckedChanged += new EventHandler(RadioButton_CheckedChanged);
+            VastausBRB.CheckedChanged += new EventHandler(RadioButton_CheckedChanged);
+            VastausCRB.CheckedChanged += new EventHandler(RadioButton_CheckedChanged);
+            VastausDRB.CheckedChanged += new EventHandler(RadioButton_CheckedChanged);
+        }
+
+        private void VastauslomakeForm_Load(object sender, EventArgs e)
+        {
+            Luettele();
+        }
+
+        private void RadioButton_CheckedChanged(Object sender, EventArgs e)
+        {
+            TyhjaaVastaus();
+            if (sender is RadioButton && Laskuri < 10)
+            {
+                RadioButton radioButton = (RadioButton)sender;
+                Vastaukset[Laskuri] = radioButton.Text;
+                Laskuri++;
+                KysymysLB.Text = "Vastaus " + Laskuri + ". kysymykseen:";
+            }
+            else
+            {
+                VastausLB.Text = "";
+                VastausARB.Enabled = false;
+                VastausBRB.Enabled = false;
+                VastausCRB.Enabled = false;
+                VastausDRB.Enabled = false;
+                for (int i = 0; i < 10; i++)
+                {
+                    if (Vastaukset[i] == OikeatVastaukset[i]) Oikein++;
+                }
+                VastausLB.Text = "Oikeita vastauksia oli: " + Oikein;
+                VastausLB.Visible = true;
+                VLB1.Visible = true;
+                Annetut = Vastaukset[0] + ", ";
+                Annetut += Vastaukset[1] + ", ";
+                Annetut += Vastaukset[2] + ", ";
+                Annetut += Vastaukset[3] + ", ";
+                Annetut += Vastaukset[4] + ", ";
+                Annetut += Vastaukset[5] + ", ";
+                Annetut += Vastaukset[6] + ", ";
+                Annetut += Vastaukset[7] + ", ";
+                Annetut += Vastaukset[8] + ", ";
+                Annetut += Vastaukset[9];
+                VLB2.Text = Annetut;
+                VLB2.Visible = true;
+            }
+            // TyhjaaVastaus();
+        }
+
+        private void TyhjaaVastaus()
+        {
+            if (VastausARB.Checked == true)
+            {
+                VastausARB.Checked = false;
+                Laskuri--;
+            }
+            if (VastausBRB.Checked == true)
+            {
+                VastausBRB.Checked = false;
+                Laskuri--;
+            }
+            if (VastausCRB.Checked == true)
+            {
+                VastausCRB.Checked = false;
+                Laskuri--;
+            }
+            if (VastausDRB.Checked == true)
+            {
+                VastausDRB.Checked = false;
+                Laskuri--;
+            }
+        }
+
+        private void Luettele()
+        {
+            Oikeat = OikeatVastaukset[0] + ", ";
+            Oikeat += OikeatVastaukset[1] + ", ";
+            Oikeat += OikeatVastaukset[2] + ", ";
+            Oikeat += OikeatVastaukset[3] + ", ";
+            Oikeat += OikeatVastaukset[4] + ", ";
+            Oikeat += OikeatVastaukset[5] + ", ";
+            Oikeat += OikeatVastaukset[6] + ", ";
+            Oikeat += OikeatVastaukset[7] + ", ";
+            Oikeat += OikeatVastaukset[8] + ", ";
+            Oikeat += OikeatVastaukset[9];
+            LB2.Text = Oikeat;
+        }
+    }
+}
