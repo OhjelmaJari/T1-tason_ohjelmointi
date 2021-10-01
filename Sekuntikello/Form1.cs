34f47dca4519b8852f993cb7e482a77f607ce8e5
Jari Kajanus <90889578+OhjelmaJari@users.noreply.github.com>
2021-10-01 17:33:15 +0300

    1.10.2021

---
+++ Sekuntikello/Form1.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Diagnostics;
+
+namespace Sekuntikello
+{
+    public partial class SekuntikelloForm : Form
+    {
+        private Stopwatch sekkari;
+        public SekuntikelloForm()
+        {
+            InitializeComponent();
+        }
+
+        private void KaynnistaBT_Click(object sender, EventArgs e)
+        {
+            sekkari.Start();
+        }
+
+        private void LopetaBT_Click(object sender, EventArgs e)
+        {
+            sekkari.Stop();
+        }
+
+        private void TyhjennaBT_Click(object sender, EventArgs e)
+        {
+            sekkari.Reset();
+        }
+
+        private void SekuntikelloForm_Load(object sender, EventArgs e)
+        {
+            sekkari = new Stopwatch();
+        }
+
+        private void AjanottoTM_Tick(object sender, EventArgs e)
+        {
+            AikaLB.Text = string.Format("{0:hh\\:mm\\:ss\\:fff}", sekkari.Elapsed);
+        }
+    }
+}
